275e4b626539658d3ec2a3065d30da1bbf091ad9
John Mathon <john_mathon@yahoo.com>
2015-07-06 14:12:48 -0700

    initial project version

--- Lynx7000.security.zwave.iot.js
+++ Lynx7000.security.zwave.iot.js
@@ -1,25 +1,18 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1347\cocoasubrtf570
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww17760\viewh19880\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+/*
+ *         Device:   Lynx7000 Console
+           Service:   TotalConnect2
+           Device:     Tilt Sensor
+            Device:     Garage Controller
+           Device:       Alarm
+           Device:      Door Sensors
+           Device:      Yale Deadbolt Lock
 
-\f0\fs24 \cf0 /**\
- *         Device:   Lynx7000 Console\
-           Service:   TotalConnect2\
-           Device:     Tilt Sensor\
-            Device:     Garage Controller\
-           Device:       Alarm\
-           Device:      Door Sensors\
-           Device:      Yale Deadbolt Lock\
-\
-Planned additional:\
-	Device:      Somfy drape controller\
-	Device:	Surveillance Cameras\
-\
- */\
-\
-/**\
- * \
- */\
-}
+Planned additional:
+	Device:      Somfy drape controller
+	Device:	Surveillance Cameras
+
+ */
+
+/**
+ * 
+ */
